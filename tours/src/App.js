91959fe5636316f3b6a7635555ace4a003579f82
Aaron Harris <aaronsharris@gmail.com>
2022-02-23 15:12:03 -0600

    added conditional return to app.js, added fetch data and useEffect to app.js

--- tours/src/App.js
+++ tours/src/App.js
@@ -4,9 +4,40 @@ import Tours from "./Tours";
 
 const url = "https://course-api.com/react-tours-project";
 
-
 function App() {
-  return <h2>Tours Project Setup</h2>;
+  const [loading, setLoading] = useState(true);
+  const [tours, setTrours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const res = await fetch(url);
+      const tours = await res.json();
+      setLoading(false)
+      setTrours(tours)
+    } catch (error) {
+      setLoading(false)
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
 export default App;
